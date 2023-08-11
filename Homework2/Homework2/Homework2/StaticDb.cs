f0e9a529bba5340fd4450501ea866ce352f4f040
Biohazardx44 <nikolailievski420@gmail.com>
2023-08-11 22:53:47 +0200

    Web-API Query Parameters

---
+++ Homework2/Homework2/Homework2/StaticDb.cs
@@ -0,0 +1,26 @@
+﻿using Homework2.Models;
+
+namespace Homework2
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books { get; } = new List<Book>
+        {
+            new Book
+            {
+                Author = "Author1",
+                Title = "Book Title 1"
+            },
+            new Book
+            {
+                Author = "Author2",
+                Title = "Book Title 2"
+            },
+            new Book
+            {
+                Author = "Author3",
+                Title = "Book Title 3"
+            }
+        };
+    }
+}
